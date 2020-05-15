72efcc6818d95a2306c68bbd97be9ed06aea6e18
Vladimir Baryshnikov <vbaryshnikov@themeetgroup.com>
2020-05-15 14:33:35 -0700

    BUG: visibilityMode=ignore is not working correctly
    There is multiple issues here.
    
    1. Visibility changed regardless, run the app and observe logcat:
    
    I/System.out: CustomLayout.setVisibility=0
    I/System.out: CustomLayout.setVisibility=0
    I/System.out: CustomLayout.setVisibility=0
    
    MotionLayout changed visibility 3 times
    
    2. Changing view visibility is instantly reverted
    
    I/System.out: CustomLayout.setVisibility=8
    I/System.out: CustomLayout.setVisibility=0

---
+++ app/src/main/java/com/example/motionlayoutexample/CustomLayout.kt
@@ -0,0 +1,17 @@
+package com.example.motionlayoutexample
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class CustomLayout @JvmOverloads constructor(
+    context: Context,
+    attrSet: AttributeSet,
+    defStyle: Int = 0
+) : FrameLayout(context, attrSet, defStyle) {
+
+    override fun setVisibility(visibility: Int) {
+        super.setVisibility(visibility)
+        println("CustomLayout.setVisibility=$visibility")
+    }
+}
--- app/src/main/java/com/example/motionlayoutexample/MainActivity.kt
+++ app/src/main/java/com/example/motionlayoutexample/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.motionlayoutexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Handler().postDelayed(
+            { findViewById<View>(R.id.customlayout).visibility = View.GONE },
+            2000
+        )
     }
 }
